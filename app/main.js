6508a677cdc19b6fed8880449194c165539f160b
Hannes Hochreiner <hannes@hochreiner.net>
2014-12-18 08:27:33 +0100

    Added html view and pouchdb backend.

--- app/main.js
+++ app/main.js
@@ -3,7 +3,9 @@ require.config({
     knockout: '../lib/knockout/knockout-3.2.0',
     jquery: '../lib/jquery/jquery-2.1.1.min',
     domReady: '../lib/require/domReady',
-    text: '../lib/require/text'
+    text: '../lib/require/text',
+    q: '../lib/q/q',
+    pouchdb: '../lib/pouchdb/pouchdb-3.2.0.min'
   }
 });
 
@@ -11,15 +13,17 @@ require([
   'knockout',
   'repo',
   'utils',
+  'pouchdb',
   'domReady!'
-], function(ko, Repo, Utils) {
+], function(ko, Repo, Utils, PouchDb) {
   ko.components.register('notes-list', { require: 'components/notesList' });
   
   function AppViewModel(repo) {
     var that = this;
     
     that.repo = repo;
+    that.ko = ko;
   }
 
-  ko.applyBindings(new AppViewModel(new Repo(new Utils())));
+  ko.applyBindings(new AppViewModel(new Repo(new Utils(), new PouchDb('project-template'))));
 });
--- app/repo.js
+++ app/repo.js
@@ -1,7 +1,40 @@
 define([
-  'jquery'
-], function($) {
-  return function(utils) {
+  'q'
+], function(Q) {
+  return function(utils, pouchdb) {
     var that = this;
+    
+    var notes = [
+      {
+        _id: 0,
+        text: 'testnote 1'
+      },
+      {
+        _id: 1,
+        text: 'testnote 2'
+      }
+    ];
+    
+    that.getAllNotes = function() {
+      var def = Q.defer();
+      
+      pouchdb.allDocs({include_docs: true}).then(function(docs) {
+        def.resolve(docs.rows.map(function(entry) {
+          return entry.doc;
+        }));
+      });
+      
+      return def.promise;
+    };
+    
+    that.addNote = function(note) {
+      return pouchdb.post(note).then(function(res) {
+        return pouchdb.get(res.id);
+      });
+    };
+    
+    that.deleteNote = function(note) {
+      return pouchdb.remove(note);
+    };
   };
 });
